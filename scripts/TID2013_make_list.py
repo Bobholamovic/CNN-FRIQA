879659130a019df91db311e1682e353cad7dd940
Bobholamovic <bob1998425@hotmail.com>
2019-05-09 13:21:14 +0800

    ..

--- scripts/TID2013_make_list.py
+++ scripts/TID2013_make_list.py
@@ -1,11 +1,11 @@
-## A script to make data lists for pytorch code
-## Database: TID2013
-## Date: 2018-11-6
-## 
-## Edited: 2019-5-7
-## Change log:
-## 	+ txt -> json
-##	+ mos saved as float values
+# A script to make data lists for pytorch code
+# Database: TID2013
+# Date: 2018-11-6
+# 
+# Edited: 2019-5-7
+# Change log:
+#    + txt -> json
+#    + mos saved as float values
 
 import random
 import json
@@ -40,49 +40,47 @@ random.shuffle(idcs)
 train_idcs = idcs[:int(N*TRAIN_RATIO)]
 val_idcs = idcs[int(N*TRAIN_RATIO):-int(N*TEST_RATIO)]
 test_idcs = idcs[-int(N*TEST_RATIO):]
+'''
 
 train_idcs = idcs[:15]
 val_idcs = idcs[15:20]
 test_idcs = idcs[20:]
 
+'''
 test_idcs = [8, 19]
 val_idcs = [6, 10, 12]
 train_idcs = [n for n in idcs if n not in test_idcs+val_idcs]
 '''
 
-train_idcs = idcs[:20]
-val_idcs = idcs[20:]
-test_idcs = idcs[23:]
-
 
 def _write_list_into_file(l, f):
-	with open(f, "w") as h:
-		for line in l:
-			h.write(line)
-			h.write('\n')
+    with open(f, "w") as h:
+        for line in l:
+            h.write(line)
+            h.write('\n')
 
 train_images, train_labels, train_mos = [], [], []
 val_images, val_labels, val_mos = [], [], []
 test_images, test_labels, test_mos = [], [], []
 
 for mos, image in data_list:
-	ref = REF_DIR + "I" + image[1:3] + '.BMP'
-	img = DATA_DIR + image
-	idx = int(image[1:3])
-	tpe = int(image[4:6])
-	if idx not in EXCLUDE_INDICES and tpe not in EXCLUDE_TYPES:
-		if idx in train_idcs:
-			train_images.append(img)
-			train_labels.append(ref)
-			train_mos.append(float(mos))
-		if idx in val_idcs:
-			val_images.append(img)
-			val_labels.append(ref)
-			val_mos.append(float(mos))
-		if idx in test_idcs:
-			test_images.append(img)
-			test_labels.append(ref)
-			test_mos.append(float(mos))			
+    ref = REF_DIR + "I" + image[1:3] + '.BMP'
+    img = DATA_DIR + image
+    idx = int(image[1:3])
+    tpe = int(image[4:6])
+    if idx not in EXCLUDE_INDICES and tpe not in EXCLUDE_TYPES:
+        if idx in train_idcs:
+            train_images.append(img)
+            train_labels.append(ref)
+            train_mos.append(float(mos))
+        if idx in val_idcs:
+            val_images.append(img)
+            val_labels.append(ref)
+            val_mos.append(float(mos))
+        if idx in test_idcs:
+            test_images.append(img)
+            test_labels.append(ref)
+            test_mos.append(float(mos))            
 
 """
 _write_list_into_file(train_images, TRAIN_IMAGES)
@@ -100,9 +98,9 @@ _write_list_into_file(test_mos, TEST_MOS)
 
 ns = vars()
 for ph in ('train', 'val', 'test'):
-	data_dict = dict(img=ns['{}_images'.format(ph)], ref=ns['{}_labels'.format(ph)], score=ns['{}_mos'.format(ph)])
-	with open('{}_data.json'.format(ph), 'w') as fp:
-		json.dump(data_dict, fp)
+    data_dict = dict(img=ns['{}_images'.format(ph)], ref=ns['{}_labels'.format(ph)], score=ns['{}_mos'.format(ph)])
+    with open('{}_data.json'.format(ph), 'w') as fp:
+        json.dump(data_dict, fp)
 
 
 
--- scripts/Waterloo_create_list.py
+++ scripts/Waterloo_create_list.py
@@ -2,7 +2,6 @@
 
 import os
 import random
-import re
 import json
 from tqdm import tqdm
 from glob import glob
@@ -27,36 +26,36 @@ LEVELS = (2,3,4)
 N_IMGS_PER_LAB = len(TYPES)*len(LEVELS)
 
 def gen_lists(labels):
-	image_list, label_list, score_list = [], [], []
-	for label in tqdm(labels):
-		im_ref = io.imread(label)
-		if im_ref.ndim != 3 or im_ref.shape[-1] != 3:
-			continue
-		label_list.extend([label]*N_IMGS_PER_LAB)
-		name, ext = os.path.splitext(os.path.basename(label))
-		cur_images = [
-		IMAGE_DIR+'_'.join([name, str(t), str(l)])+ext
-		for t in TYPES
-		for l in LEVELS
-	]
-		image_list.extend(cur_images)
-		score_list.extend([compute_score(io.imread(dst), im_ref) for dst in cur_images])
+    image_list, label_list, score_list = [], [], []
+    for label in tqdm(labels):
+        im_ref = io.imread(label)
+        if im_ref.ndim != 3 or im_ref.shape[-1] != 3:
+            continue
+        label_list.extend([label]*N_IMGS_PER_LAB)
+        name, ext = os.path.splitext(os.path.basename(label))
+        cur_images = [
+        IMAGE_DIR+'_'.join([name, str(t), str(l)])+ext
+        for t in TYPES
+        for l in LEVELS
+    ]
+        image_list.extend(cur_images)
+        score_list.extend([compute_score(io.imread(dst), im_ref) for dst in cur_images])
 
-	return image_list, label_list, score_list
+    return image_list, label_list, score_list
 
 
 def compute_score(im_dst, im_ref):
-	def _to_tensor(arr):
-		return torch.FloatTensor(arr).transpose(-1,-2).transpose(-2,-3).unsqueeze(0)
-	with torch.no_grad():
-		if torch.cuda.is_available():
-			score = metric(_to_tensor(im_dst).cuda(), _to_tensor(im_ref).cuda())
-		else:
-			score = metric(_to_tensor(im_dst), _to_tensor(im_ref))
-	return float(score.cpu())
+    def _to_tensor(arr):
+        return torch.FloatTensor(arr).transpose(-1,-2).transpose(-2,-3).unsqueeze(0)
+    with torch.no_grad():
+        if torch.cuda.is_available():
+            score = metric(_to_tensor(im_dst).cuda(), _to_tensor(im_ref).cuda())
+        else:
+            score = metric(_to_tensor(im_dst), _to_tensor(im_ref))
+    return float(score.cpu())
 
 def _get(*args):
-	return globals().get('_'.join(args), [])    
+    return globals().get('_'.join(args), [])    
 
 # Load names
 label_list = glob(os.path.join(LABEL_DIR, '*.bmp'))
@@ -78,13 +77,13 @@ val_labels = label_list[-20:]
 test_labels = label_list[-10:]
 
 for ph in ('train', 'val', 'test'):
-	data_zip = zip(('img', 'ref', 'score'), gen_lists(_get(ph, 'labels')))
-	data_dict = dict(data_zip)
-	with open('{}_data.json'.format(ph), 'w') as fp:
-		json.dump(data_dict, fp)
+    data_zip = zip(('img', 'ref', 'score'), gen_lists(_get(ph, 'labels')))
+    data_dict = dict(data_zip)
+    with open('{}_data.json'.format(ph), 'w') as fp:
+        json.dump(data_dict, fp)
 
 
-	
-		
-			
-	
+    
+        
+            
+    
